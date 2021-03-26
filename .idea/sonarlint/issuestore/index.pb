93332e78d3ab790c472b944f335a35a80649a4cc
Vladdosiik <vladdosiik7540@gmail.com>
2021-03-26 11:36:06 +0300

    start

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/CalculatorApp.java,6\4\64d5592ee3b75b6220ee6b767893328bb28d4786
---
+++ src/main/java/CalculatorApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        System.out.println("Введите выражение");
+        Scanner scanner=new Scanner(System.in);
+        String inputExpression=scanner.next();
+        
+    }
+}
